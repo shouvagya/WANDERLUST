292789f45c8fa914630e1bd12f233020507b0035
shouvagya <shouvagyasaha@gmail.com>
2025-12-29 15:46:47 +0530

    Fixed Home page

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ const sessionOptions = {
 
 
 
-// app.get("/",(req,res)=>{
-//     res.send("Hi ,I am root");
-// });
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
